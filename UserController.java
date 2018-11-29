61ebe1c9a21e28e9da2eefa8cc9669c465f15be0
haihong <19951001chh@126.com>
2018-11-29 10:20:46 +0800

    modify by

--- UserController.java
+++ UserController.java
@@ -3,7 +3,7 @@ public class TestGit{
 	public static void main(String[] airgs){
 		System.out.println("hello world");
 		System.out.println("hello");
-		我会哈哈哈 edit by chh19951001
+		我会哈哈哈 modify by haihong123
 		
 	}
 
